52340bd1bd9f18f5deeec78836e1bb31d6961480
Bono Fox <git@bono-fox.de>
2025-10-22 15:54:31 +0200

    ⚡️ #58: Separated GUI- and CLI-features
    - optional feature dependencies for each binary
    - only necessary dependencies are downloaded
    - optimizes especially the cli-version

--- build.rs
+++ build.rs
@@ -1,8 +1,19 @@
-use std::fs;
-use std::path::Path;
-use tiny4linux_assets::absolute_path_for_t4l_asset;
-
 fn main() {
+    let gui_feature_enabled = std::env::var("CARGO_FEATURE_GUI").is_ok();
+
+    if !gui_feature_enabled {
+        return;
+    }
+
+    build_gui_assets();
+}
+
+#[cfg(feature = "gui")]
+fn build_gui_assets() {
+    use std::fs;
+    use std::path::Path;
+    use tiny4linux_assets::absolute_path_for_t4l_asset;
+
     let asset_src = absolute_path_for_t4l_asset("generated/png/title-icon/v2.0-soft-shadow.png");
 
     let out_dir = std::env::var("OUT_DIR").unwrap();
@@ -16,4 +27,9 @@ fn main() {
     fs::copy(&asset_src, &target_dir).unwrap();
 
     println!("cargo:rerun-if-changed={}", asset_src.display());
+}
+
+#[cfg(not(feature = "gui"))]
+fn build_gui_assets() {
+    // Noop
 }
