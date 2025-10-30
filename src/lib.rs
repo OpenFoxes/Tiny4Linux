e3ad6ae7cb565287cfaf6b7c6e5e6bc7932a529b
Bono Fox <git@bono-fox.de>
2025-10-30 16:05:57 +0100

    ♻️ Added Display names for exposure modes

--- src/lib.rs
+++ src/lib.rs
@@ -185,6 +185,16 @@ pub enum ExposureMode {
     Face,
 }
 
+impl Display for ExposureMode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ExposureMode::Manual => write!(f, "Manual"),
+            ExposureMode::Global => write!(f, "Global"),
+            ExposureMode::Face => write!(f, "Face"),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum ExposureModeType {
     Auto,
