712e9bf5423e54c892a7d2151db9f15c83f02f6e
Bono Fox <git@bono-fox.de>
2025-10-31 13:44:31 +0100

    💄 New Widget Desktop Icon

--- build.rs
+++ build.rs
@@ -10,26 +10,34 @@ fn main() {
 
 #[cfg(feature = "gui")]
 fn build_gui_assets() {
-    use std::fs;
-    use std::path::Path;
-    use tiny4linux_assets::absolute_path_for_t4l_asset;
-
-    let asset_src = absolute_path_for_t4l_asset("generated/png/title-icon/v2.0-soft-shadow.png");
-
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let target_dir = Path::new(&out_dir)
-        .ancestors()
-        .nth(3)
-        .unwrap()
-        .join("assets/icon.png");
-
-    fs::create_dir_all(target_dir.parent().unwrap()).unwrap();
-    fs::copy(&asset_src, &target_dir).unwrap();
-
-    println!("cargo:rerun-if-changed={}", asset_src.display());
+    copy_asset(
+        "generated/png/title-icon/v2.0-soft-shadow.png",
+        "assets/icon.png",
+    );
+    copy_asset(
+        "generated/png/title-icon/v2.0-widget.png",
+        "assets/icon-widget.png",
+    );
 }
 
 #[cfg(not(feature = "gui"))]
 fn build_gui_assets() {
     // Noop
+}
+
+#[cfg(feature = "gui")]
+fn copy_asset(src: &str, dst: &str) {
+    use std::fs;
+    use std::path::Path;
+    use tiny4linux_assets::absolute_path_for_t4l_asset;
+
+    let asset_src = absolute_path_for_t4l_asset(src);
+
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let target_dir = Path::new(&out_dir).ancestors().nth(3).unwrap().join(dst);
+
+    fs::create_dir_all(target_dir.parent().unwrap()).unwrap();
+    fs::copy(&asset_src, &target_dir).unwrap();
+
+    println!("cargo:rerun-if-changed={}", asset_src.display());
 }
