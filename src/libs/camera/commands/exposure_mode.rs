5e204f15a06205c4b200fa6f0e6aaaad1a8790e1
Bono Fox <git@bono-fox.de>
2025-11-27 18:43:42 +0100

    🚧 WIP

--- src/libs/camera/commands/exposure_mode.rs
+++ src/libs/camera/commands/exposure_mode.rs
@@ -13,3 +13,38 @@ impl ExposureModeCommand {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    mod unit {
+        use crate::{ExposureMode, ExposureModeCommand};
+        use assertables::{assert_none, assert_some};
+        use test_case::test_case;
+
+        #[test]
+        fn manual_exposure_mode() {
+            let exposure_command = ExposureModeCommand::build(ExposureMode::Manual);
+
+            assert_none!(
+                exposure_command,
+                "manual exposure mode should not set a exposure mode command"
+            )
+        }
+
+        #[test_case(ExposureMode::Global, [0x03, 0x01, 0x00]; "global automatic exposure should be set")]
+        #[test_case(ExposureMode::Face, [0x03, 0x01, 0x01]; "face automatic exposure should be set")]
+        fn automatic_exposure_modes(mode: ExposureMode, expected: [u8; 3]) {
+            let exposure_command = ExposureModeCommand::build(mode);
+
+            assert_some!(
+                exposure_command,
+                "automatic exposure mode should set a exposure mode command"
+            );
+            assert_eq!(
+                exposure_command.unwrap(),
+                expected,
+                "correct command is expected"
+            )
+        }
+    }
+}
--- src/libs/camera/commands/sleep.rs
+++ src/libs/camera/commands/sleep.rs
@@ -35,3 +35,39 @@ impl SleepCommand {
             .build())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{SleepCommand, SleepMode};
+    use assertables::assert_ok;
+    use test_case::test_case;
+
+    #[test_case(SleepMode::Awake, [0xa5, 0x00], [0x5f, 0xef], [0xbe, 0x07, 0x00, 0x00, 0x00, 0x00]; "awake command")]
+    #[test_case(SleepMode::Sleep, [0x42, 0x00], [0xea, 0x63], [0xbf, 0xfb, 0x01, 0x00, 0x00, 0x00]; "sleep command")]
+    fn sleep_mode(mode: SleepMode, sequence_nr: [u8; 2], checksum: [u8; 2], command: [u8; 6]) {
+        let sleep_command_option = SleepCommand::build(mode);
+
+        assert_ok!(&sleep_command_option, "sleep mode command should be built");
+
+        let sleep_command = sleep_command_option.unwrap();
+        assert_eq!(
+            sleep_command[8..14],
+            [0x0a, 0x02, 0xc2, 0xa0, 0x04, 0x00],
+            "Function group should be set for sleep mode"
+        );
+        assert_eq!(
+            sleep_command[2..4],
+            sequence_nr,
+            "Sequence number should be set"
+        );
+        assert_eq!(sleep_command[6..8], checksum, "Checksum should be set");
+        assert_eq!(sleep_command[14..20], command, "Command should be set");
+    }
+
+    #[test]
+    #[should_panic]
+    fn unknown_sleep_mode() {
+        SleepCommand::build(SleepMode::Unknown)
+            .expect("Should panic because of unknown sleep mode");
+    }
+}
