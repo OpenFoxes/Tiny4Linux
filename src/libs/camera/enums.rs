fcb6a56872cfea92c16c8e785423641e537240b3
Bono Fox <git@bono-fox.de>
2025-11-27 17:19:45 +0100

    ✅ #106: Added tests for Enum Displays

--- src/libs/camera/enums.rs
+++ src/libs/camera/enums.rs
@@ -89,3 +89,65 @@ pub enum ExposureModeType {
     Auto,
     Manual,
 }
+
+#[cfg(test)]
+mod tests {
+    mod unit {
+        mod display {
+            mod sleep_mode {
+                use crate::SleepMode;
+                use test_case::test_case;
+
+                #[test_case(SleepMode::Awake, "Awake"; "Awake")]
+                #[test_case(SleepMode::Sleep, "Sleeping"; "Sleep")]
+                #[test_case(SleepMode::Unknown, "Unknown"; "Unknown")]
+                fn sleep_mode(mode: SleepMode, expected: &str) {
+                    assert_eq!(&mode.to_string(), expected);
+                }
+            }
+
+            mod ai_mode {
+                use crate::AIMode;
+                use test_case::test_case;
+
+                #[test_case(AIMode::NoTracking, "Static"; "no tracking")]
+                #[test_case(AIMode::NormalTracking, "Normal Tracking"; "normal tracking")]
+                #[test_case(AIMode::UpperBody, "Upper Body"; "upper body")]
+                #[test_case(AIMode::CloseUp, "Close-up"; "close up")]
+                #[test_case(AIMode::Headless, "Headless"; "headless")]
+                #[test_case(AIMode::LowerBody, "Lower Body"; "lower body")]
+                #[test_case(AIMode::DeskMode, "Desk Mode"; "desk mode")]
+                #[test_case(AIMode::Whiteboard, "Whiteboard"; "whiteboard")]
+                #[test_case(AIMode::Hand, "Hand"; "hand")]
+                #[test_case(AIMode::Group, "Group"; "group")]
+                #[test_case(AIMode::Unknown, "Unknown"; "unknown")]
+                fn ai_mode(mode: AIMode, expected: &str) {
+                    assert_eq!(&mode.to_string(), expected);
+                }
+            }
+
+            mod tracking_speed {
+                use crate::TrackingSpeed;
+                use test_case::test_case;
+
+                #[test_case(TrackingSpeed::Standard, "Standard"; "standard")]
+                #[test_case(TrackingSpeed::Sport, "Sport"; "sport")]
+                fn tracking_speed(mode: TrackingSpeed, expected: &str) {
+                    assert_eq!(&mode.to_string(), expected);
+                }
+            }
+
+            mod exposure_mode {
+                use crate::ExposureMode;
+                use test_case::test_case;
+
+                #[test_case(ExposureMode::Manual, "Manual"; "manual")]
+                #[test_case(ExposureMode::Global, "Global"; "global")]
+                #[test_case(ExposureMode::Face, "Face"; "face")]
+                fn exposure_mode(mode: ExposureMode, expected: &str) {
+                    assert_eq!(&mode.to_string(), expected);
+                }
+            }
+        }
+    }
+}
